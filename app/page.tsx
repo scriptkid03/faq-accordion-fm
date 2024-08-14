5e1ed174b60cb18a1335f4be95541a679b62b26d
Edmund Phillips <edmund.phillips@acity.edu.gh>
2024-08-13 22:12:09 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-24">
+      FAQ Accordion
+    </main>
+  );
+}
